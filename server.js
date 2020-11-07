97cb03e8d95adbdfc86e0722e77763434a31a490
SUNNY TIWARI <37635908+sunny-Ne5@users.noreply.github.com>
2020-11-08 02:33:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 // server.js
-// where your node app starts
+
 
 // init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,10 +17,7 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  console.log(req.ip);
-  console.log(req.headers);
   res.json({greeting: 'hello API'});
 });
 
